f910ee218521f23f50ce2028690311134813097c
Yevhenii Vyshnevskyi <rochithefox@gmail.com>
2023-10-23 15:49:03 +0300

    Added: added rotating platform

--- Source/ObstacleAssault/MovingPlatform.cpp
+++ Source/ObstacleAssault/MovingPlatform.cpp
@@ -54,7 +54,7 @@ void AMovingPlatform::MovePlatform(float DeltaTime)
 
 void AMovingPlatform::RotatePlatform(float DeltaTime)
 {
-	UE_LOG(LogTemp, Display, TEXT("%s Rotating..."), *GetName());
+	AddActorLocalRotation(RotationVelocity * DeltaTime);
 }
 
 float AMovingPlatform::GetDistanceMoved() const
